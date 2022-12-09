379d48badc2838dffad5e638e612dcc66c46ed54
Carolina <caro.frontend@gmail.com>
2022-12-09 18:14:01 -0300

    Refactor the code so that it consists of three new components: Header, Content, and Total

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Header = (props) => {
+  return (
+    <div>
+      <p>The name of this course is {props.course}</p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        Part 1: {props.part1}
+        number of exercises: {props.exercises1}
+      </p>
+      <p>
+        Part 2: {props.part2}
+        number of exercises: {props.exercises2}
+      </p>
+      <p>
+        Part 3: {props.part3}
+        number of exercises: {props.exercises3}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises:{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -12,17 +50,20 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 };

04710815c7a4850c9c4f6290a048fd4bbdb746e8
Carolina <caro.frontend@gmail.com>
2022-12-13 16:05:34 -0300

    new content Component state and  event handlers

--- src/index.js
+++ src/index.js
@@ -1,69 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = (props) => {
-  return (
-    <div>
-      <p>The name of this course is {props.course}</p>
-    </div>
-  );
-};
-
-const Content = (props) => {
+const Hello = (props) => {
   return (
     <div>
       <p>
-        Part 1: {props.part1}
-        number of exercises: {props.exercises1}
-      </p>
-      <p>
-        Part 2: {props.part2}
-        number of exercises: {props.exercises2}
-      </p>
-      <p>
-        Part 3: {props.part3}
-        number of exercises: {props.exercises3}
-      </p>
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises:{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
+        Hello {props.name}, you are {props.age} years old
       </p>
     </div>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const name = "Peter";
+  const age = 10;
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 };

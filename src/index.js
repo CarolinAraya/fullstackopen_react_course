069815181ef8486afe8916702aaadb48be7c2d13
Carolina <caro.frontend@gmail.com>
2022-12-14 10:27:03 -0300

    new content, page re-rendering

--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
+const App = (props) => {
+  const { counter } = props;
+  return <div>{counter}</div>;
 };
 
-const App = () => {
-  const name = "Peter";
-  const age = 10;
+let counter = 1;
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  );
-};
+ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
 
 ReactDOM.render(<App />, document.getElementById("root"));
